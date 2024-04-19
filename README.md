6d431ce66f35b406dea76df0865d5aad46f24fcf
Hakizimana-fred <hakifred20@gmail.com>
2024-04-19 12:24:57 +0300

    scaffold ui

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Todo
 
--[ ] make it deploy -[ ] Scafold basic ui with mock data -[ ] Actually set up a database (vercel postgres) -[ ] Attach database to UI -[ ] Add authentication (w/ clerk) -[ ] Add image upload -[ ] Routing/image page (parell route) -[ ] Delete button (w/ Server actions) -[ ] Analytics (posthog) -[ ] Ratelimiting (upstash)
+-[x] make it deploy -[x] Scafold basic ui with mock data -[x] Actually set up a database (vercel postgres) -[ ] Attach database to UI -[ ] Add authentication (w/ clerk) -[ ] Add image upload -[ ] Routing/image page (parell route) -[ ] Delete button (w/ Server actions) -[ ] Analytics (posthog) -[ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls: string[] = [
+  'https://utfs.io/f/ce171a1f-f9a2-4527-a12b-98b158d14c95-gx0l0w.jpg',
+  'https://utfs.io/f/d5dd6a1c-a3e5-44c8-b590-8bed615fde9f-h2f60z.jpg',
+  'https://utfs.io/f/62f7afe8-49f1-4b2b-8211-090049168f46-h2f6sh.jpg',
+  'https://utfs.io/f/ecd696a7-a6af-4681-8d80-e52f371e7639-x8sigy.jpg',
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url 
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Gallery in progress)
+    <main className="">
+     <div className="flex flex-wrap gap-4">
+      {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt="" />
+        </div>
+      ))}
+     </div>
     </main>
   );
 }
