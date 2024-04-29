b893b79b63e2dc21a506704a14e8922a1b08b6c4
Hakizimana-fred <hakifred20@gmail.com>
2024-04-29 15:39:49 +0300

    use next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { db } from "~/server/db";
+import { getImages } from "~/server/queries";
 
 
 export const dynamic ="force-dynamic"
 
 async function Images() {
-const posts = await db.query.posts.findMany({
-    orderBy: (model, {desc}) => desc(model.id)
-  })
 
+  const posts = await getImages()
   return (
 
-     <div className="flex flex-wrap gap-4">
+     <div className="flex flex-wrap justify-center gap-4">
       {posts.map((post, idx) => (
-        <div key={idx} className="flex w-48 flex-col">
-          <img src={post.url} alt="" />
+        <div key={idx} className="flex w-48 h-48 flex-col">
+          <Image style={{objectFit: 'contain'}} src={post.url} alt={post.name} width={192} height={192} />
         </div>
       ))}
      </div>
